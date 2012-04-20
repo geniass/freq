cced01c12c710633facc2fe8c1265579bf87313d
Daniel Levin <daniellevin2607@gmail.com>
2012-04-20 21:52:19 +0200

    modular redesign

--- freq.py
+++ freq.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
-import sqlite3
+import sqlite3 as sqlite
 
 # ==============================================================================
 # Database Interface Code ======================================================
 # ==============================================================================
+
+class Database:
+
+    def __init__(self):
+        try:
+            self.con = sqlite.connect("words.db")
+            self.cur = self.con.cursor()
+        except sqlite.Error, e:
+            print e
+
+    def close(self):
+        if self.con:
+            self.con.close()
+
+
+db = Database()
+db.close()
