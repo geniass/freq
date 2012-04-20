45bea863950a2493c0fd419a3a73f6ebd98ddd33
Daniel Levin <daniellevin2607@gmail.com>
2012-04-20 21:37:16 +0200

    throwaway

--- freq.py
+++ freq.py
@@ -5,15 +5,3 @@ import sqlite3
 # ==============================================================================
 # Database Interface Code ======================================================
 # ==============================================================================
-
-def load_database():
-    try:
-        conn = sqlite3.connect("words.db")
-    except:
-        print("Error")
-
-# Essentially main
-def bootstrap():
-    load_database()
-
-bootstrap()
