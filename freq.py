994be3f8874e496fb602876aee479ba09159c77c
Daniel Levin <daniellevin2607@gmail.com>
2012-04-20 19:58:13 +0200

    Database interface skeleton added

--- freq.py
+++ freq.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 
 import sqlite3
+
+# ==============================================================================
+# Database Interface Code ======================================================
+# ==============================================================================
+
+def load_database():
+    try:
+        conn = sqlite3.connect("words.db")
+    except:
+        print("Error")
+
+# Essentially main
+def bootstrap():
+    load_database()
+
+bootstrap()
